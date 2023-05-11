655e658416b84eecd6ff086fc10033ebae1206f7
flavia-Arroyo <flavia.e.arroyo@gmail.com>
2023-05-11 15:02:25 -0300

    activacion paginas

--- js/activePage.js
+++ js/activePage.js
@@ -1,10 +1,10 @@
 /*-----------indicar la pagina que se visita-----------*/
 
 const activePage = window.location.pathname;
-
+console.log(activePage)
 
 const navLinks = document.querySelectorAll('nav a').forEach(link=>{
-    console.log(navLinks)
+    console.log(document.querySelectorAll('nav a'))
     if(link.href.includes(`${activePage}`)){
         link.classList.add('active');
        
