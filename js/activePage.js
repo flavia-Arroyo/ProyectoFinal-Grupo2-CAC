403b195035c1af30629a8e86868f092c3ad0e297
flavia-Arroyo <flavia.e.arroyo@gmail.com>
2023-05-11 15:58:51 -0300

    correccion act

--- js/activePage.js
+++ js/activePage.js
@@ -1,10 +1,11 @@
 /*-----------indicar la pagina que se visita-----------*/
 
-const activePage = window.location.href;
+const activePage = window.location.pathname;
 console.log(activePage)
 
 const navLinks = document.querySelectorAll('nav a').forEach(link=>{
     console.log(document.querySelectorAll('nav a'))
+
     if(link.href.includes(`${activePage}`)){
         link.classList.add('active');
        
