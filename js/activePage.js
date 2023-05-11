536cd902f076a5f1e4780b6cd4782824febfa1c9
Flavia Arroyo <98117424+flavia-Arroyo@users.noreply.github.com>
2023-05-11 12:20:46 -0300

    Update activePage.js

--- js/activePage.js
+++ js/activePage.js
@@ -1,8 +1,10 @@
 /*-----------indicar la pagina que se visita-----------*/
 
 const activePage = window.location.pathname;
+console.log(activePage)
 
 const navLinks = document.querySelectorAll('nav a').forEach(link=>{
+    console.log(navLinks)
     if(link.href.includes(`${activePage}`)){
         link.classList.add('active');
        
