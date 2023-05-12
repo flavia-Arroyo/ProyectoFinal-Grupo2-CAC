70aff142eb0cadfd973a82cc80fd3822e1327429
Mariano <mariano@mail.com>
2023-05-11 21:11:44 -0300

    formulario validado con alertas

--- js/formulario.js
+++ js/formulario.js
@@ -16,27 +16,48 @@ function validarFormulario(e){
 
 
 form.addEventListener('submit', (event) => {
-	event.preventDefault();
-  
-	if (!nombre) {
-	  alert('Por favor ingrese su nombre');
-	  
+
+	const nombre = form.querySelector('#nombre').value;
+	const email = form.querySelector('#email').value;
+	const mensaje = form.querySelector('#mensaje').value;
+	const telefono = form.querySelector('#telefono').value;
+
+	const expresiones={
+		nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
+		email:  /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+		telefono: /^\d{7,14}$/ ,
+		mensaje: /^[a-zA-Z0-9.,;]{10,199}$/, 
+
 	}
-  
-	if (!email) {
-	  alert('Por favor ingrese su email');
-	  return;
-	}
-  
-	if (!telefono) {
-	  alert('Por favor ingrese su telefono');
-	  return;
-	}
-  
-	if (!mensaje) {
-	  alert('Por favor ingrese su mensaje');
-	  return;
-	}
-  
+
+
+	if(!expresiones.nombre.test(nombre)){
+		alert('Por favor, ingrese nombre...')
+	};
+
+	if(!expresiones.email.test(email)){
+		alert('Por favor, ingrese email...')
+	};
+	
+	if(!expresiones.telefono.test(telefono)){
+		alert('Por favor, ingrese telefono...')
+	};
+	console.log(mensaje.split(''));
+	if(!expresiones.mensaje.test(mensaje) && mensaje.split('').length<10){
+		alert('Por favor, ingrese mensaje - caracteres desde 10 a 199...')
+	};
+
+	form.querySelector('#nombre').value = '';
+	form.querySelector('#email').value = '';
+	form.querySelector('#telefono').value = '';
+	form.querySelector('#mensaje').value = '';
+
+
+	console.log(nombre);
+	console.log(email);
+	console.log(mensaje);
+	console.log(telefono);
+
+	event.preventDefault()
   });
   
